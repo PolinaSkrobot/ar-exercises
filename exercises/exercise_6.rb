f51b3d3c1b38b25547abd9719e342749515a8c05
Polina Skrobot <chi.polly.no@gmail.com>
2021-10-12 00:29:20 +0000

    Ex6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Leo", last_name: "DiCaprio", hourly_rate: 45)
+@store2.employees.create(first_name: "Kirill", last_name: "Gorskov", hourly_rate: 78)
+@store4.employees.create(first_name: "Lono", last_name: "Virani", hourly_rate: 70)
+@store5.employees.create(first_name: "Monica", last_name: "Belucci", hourly_rate: 50)
+@store6.employees.create(first_name: "Vladimir", last_name: "Suhoy", hourly_rate: 45)
